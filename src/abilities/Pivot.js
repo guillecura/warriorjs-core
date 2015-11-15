f1ed4cd959f6e833032039890a97b5e852db0c4f
Matias Olivera <moliverafreire@gmail.com>
2015-11-15 11:26:52 -0300

    Fix Pivot ability

--- src/abilities/Pivot.js
+++ src/abilities/Pivot.js
@@ -1,12 +1,19 @@
 import RelativeDirections from '../constants/RelativeDirections';
 import Base from './Base';
 
+const ROTATION_DIRECTIONS = [
+  RelativeDirections.forward,
+  RelativeDirections.right,
+  RelativeDirections.backward,
+  RelativeDirections.left,
+];
+
 class Pivot extends Base {
   _description = 'Rotate left, right or backward (default).';
 
   perform(direction = RelativeDirections.backward) {
     this.verifyDirection(direction);
-    this._unit.getPosition().rotate(direction);
+    this._unit.getPosition().rotate(ROTATION_DIRECTIONS.indexOf(direction));
     this._unit.say(`pivots ${direction}`);
   }
 }
--- test/abilities/Pivot.js
+++ test/abilities/Pivot.js
@@ -16,13 +16,13 @@ describe('Pivot', function () {
   });
 
   it('should flip around when not passing arguments', function () {
-    const expectation = this.sinon.mock(this.position).expects('rotate').withArgs('backward');
+    const expectation = this.sinon.mock(this.position).expects('rotate').withArgs(2);
     this.pivot.perform();
     expectation.verify();
   });
 
   it('should rotate right when pivoting right', function () {
-    const expectation = this.sinon.mock(this.position).expects('rotate').withArgs('right');
+    const expectation = this.sinon.mock(this.position).expects('rotate').withArgs(1);
     this.pivot.perform('right');
     expectation.verify();
   });
